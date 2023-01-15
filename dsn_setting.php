61182e1b7bfb58fb09beaabcc99f0d6c64067b34
Ryuta <ryuutafuruhashi310@gmail.com>
2023-01-15 23:24:25 +0900

    executeCouldAaiable

--- dsn_setting.php
+++ dsn_setting.php
@@ -1,54 +1,20 @@
 <?php
-$name=$_POST["name"];
-$bug=$_POST["bug"];
-$types=$_POST["types"];
-$price=$_POST["price"];
-$expiration=$_POST["expiration"];
-$stock=$_POST["stock"];
-$about=$_POST["about"];
-$from=$_POST["from"];
-
-
-$dsn='mysql:host=localhost;dbname=ei2031;charset=utf8';
-$user='ei2031';
-$password='ei2031@alumni.hamako-ths.ed.jp';
 
 try{
-    $db=new PDO($dsn,$user,$password);
-    $db->setAttribute(PDO::ATTR_EMULATE_PREPARES,false);
+$pdo = new PDO('mysql:host=localhost;dbname=ei2031','ei2031','ei2031@alumni.hamako-ths.ed.jp');
 
-    $stmt=$db->prepare(
-        "
-        insert
-        into
-        product(name,bug,type,price,expiration,about,stock) 
-        values
-        (
-            :name,
-            :bug,
-            :type,
-            :price,
-            :expiration,
-            :about,
-            :stock
-        );
-        "
-    );
+$stmt = $pdo->prepare("select * from product;");
+$result = $stmt->execute();
 
-    $stmt->bindParam(':name',$name,PDO::PARAM_STR);
-    $stmt->bindParam(':bug',$bug,PDO::PARAM_STR);
-    $stmt->bindParam(':type',$types,PDO::PARAM_INT);
-    $stmt->bindParam(':price',$price,PDO::PARAM_INT);
-    $stmt->bindParam(':expiration',$expiration,PDO::PARAM_STR);
-    $stmt->bindParam(':about',$about,PDO::PARAM_STR);
-    $stmt->bindParam(':stock',$stock,PDO::PARAM_INT);
+#$result = $pdo->query('select * from product');
 
-    $stmt->execute();
-
-    header('Location : setting.html');
-    exit();
-}catch(PDOException $e){
-    die('error:'.$e->getMessage());
+foreach($result as $text){
+	echo "{$text['name']}"."<br>";
 }
-include "verif.html";
+
+echo "true";
+}catch(PDOException $e){
+echo $e->getMessage();
+die();
+}
 ?>
