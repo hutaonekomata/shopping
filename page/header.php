dd0e8837c9d28c3c167d19506d4099ae20cc88ec
hk-pG <hukada.gogotea@gmail.com>
2023-01-23 22:53:47 +0900

    ヘッダを分離

---
+++ page/header.php
@@ -0,0 +1,11 @@
+<link rel="stylesheet" href="../css/header.css">
+<header>
+    <div class="header-contents">
+        <h1 class="title">昆虫食専門店(仮)</h1>
+        <div class="search-container">
+            <input type="text" id="search">
+            <button class="icon-button">
+            </button>
+        </div>
+    </div>
+</header>
