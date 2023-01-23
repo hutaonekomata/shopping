ff3e350d89cc3f06a4b0b5e85ea5aa5b7dc58957
hk-pG <hukada.gogotea@gmail.com>
2023-01-23 22:58:26 +0900

    cssのファイル名を変更

--- page/header.php
+++ page/header.php
@@ -2,6 +2,7 @@
     require_onceで呼び出される時、パスは呼び出し側（実行側）になるので、
     あらかじめ合わせておくか、同じ階層にファイルを置くとやりやすい。
 -->
+<link rel="stylesheet" href="../css/style.css">
 <link rel="stylesheet" href="../css/header.css">
 <header>
     <div class="header-contents">
--- page/website.php
+++ page/website.php
@@ -6,7 +6,7 @@
   <meta http-equiv="X-UA-Compatible" content="IE=edge">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <title>昆虫食専門サイト</title>
-  <link rel="stylesheet" href="../css/website.css">
+  <link rel="stylesheet" href="../css/style.css">
 </head>
 
 <body>
