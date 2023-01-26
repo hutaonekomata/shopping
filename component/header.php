27e7e25b003d9a22e6a20d5007466c39eff916fa
hk-pG <hukada.gogotea@gmail.com>
2023-01-27 00:09:23 +0900

    ヘッダの検索欄を削除し、修正

--- component/header.php
+++ component/header.php
@@ -2,18 +2,38 @@
     require_onceで呼び出される時、パスは呼び出し側（実行側）になるので、
     あらかじめ合わせておくか、同じ階層にファイルを置くとやりやすい。
 -->
+<link rel="stylesheet" href="../css/mdl/material.min.css">
+<link rel="stylesheet" href="https://fonts.googleapis.com/icon?family=Material+Icons">
+<script src="../css/mdl/material.min.js"></script>
+
+
 <link rel="stylesheet" href="../css/style.css">
 <link rel="stylesheet" href="../css/header.css">
 <header>
     <div class="header-contents">
         <a href="./home.php">
-            <h1 class="title">昆虫食専門店(仮)</h1>
+            <h1 class="title">
+                昆虫食専門店(仮)
+            </h1>
         </a>
-        <div class="search-container">
-            <input type="text" id="search">
-            <button class="icon-button">
-                検索
-            </button>
+
+        <div class="icon-container">
+
+            <a href="../page/cart.php">
+                <button
+                    class="icon-button icon-button-cart mdl-button mdl-js-button mdl-button--raised mdl-js-ripple-effect mdl-color--white">
+                    カート
+                </button>
+            </a>
+
+            <a href="../account.html">
+                <button
+                    class="icon-button icon-button-account mdl-button mdl-js-button mdl-button--raised mdl-js-ripple-effect mdl-color--white">
+                    アカウント
+                </button>
+            </a>
+
         </div>
+
     </div>
 </header>
