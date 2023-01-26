db0dc6fad582e98d9d556049d7242c065e133fa6
hk-pG <hukada.gogotea@gmail.com>
2023-01-27 00:17:14 +0900

    cart.phpでカートの中身を見れるように実装

---
+++ page/cart.php
@@ -0,0 +1,42 @@
+<!DOCTYPE html>
+<html lang="ja">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="../css/home.css">
+    <title>カート</title>
+</head>
+
+<body>
+    <?php require_once('../component/product_template.php')  ?>
+    <?php require_once('../component/header.php')  ?>
+    <?php require_once('../phpFunc/getcart.php') ?>
+
+    <main>
+        <section class="products-container">
+
+            <!-- phpでsql実行後、とってきた配列から必要な情報を抜き出し、ループで以下のhtmlに埋め込む -->
+            <?php if (empty($cart_products)) : ?>
+
+            <h2>カートが空です</h2>
+
+            <?php else : ?>
+
+            <h2>カート内の商品</h2>
+            <?php
+                // TODO $productsをカート内の商品に変える
+                foreach ($cart_products as $key => $row) {
+                    product_template($row);
+                }
+                ?>
+
+            <?php endif ?>
+
+
+        </section>
+    </main>
+</body>
+
+</html>
--- phpFunc/getcart.php
+++ phpFunc/getcart.php
@@ -1,34 +1,33 @@
 <?php
-try{
-$userID = $_COOKIE['session_id'];
-$pdo_config = 'mysql:host=localhost;dbname=ei2031';
+try {
+    $userID = $_COOKIE['session_id'];
+    $pdo_config = 'mysql:host=localhost;dbname=ei2031';
 
-$user='ei2031';
-$password='ei2031@alumni.hamako-ths.ed.jp';
-$option='[PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC]';
+    $user = 'ei2031';
+    $password = 'ei2031@alumni.hamako-ths.ed.jp';
+    $option = '[PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC]';
 
-$pdo = new PDO($pdo_config,$user,$password);
-$pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+    $pdo = new PDO($pdo_config, $user, $password);
+    $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 
-// $sql = '
-//     update user set `Email`=:email where `id`=:id;
-// ';
+    // $sql = '
+    //     update user set `Email`=:email where `id`=:id;
+    // ';
 
-$sql = '
+    $sql = '
 	select product.*,cart.num from product inner join cart on product.id = cart.productID where cart.userID=:id;
 ';
 
-$stmt = $pdo->prepare($sql);
-$stmt->bindParam(':id',$userID,PDO::PARAM_STR);
-$res = $stmt->execute();
-if($res){
-    $url=$_SERVER['HTTP_REFERER'];
-    header('Location:'.$url);
-}
-$pdo=null;
-exit();
-}catch(PDOException $e){
+    $stmt = $pdo->prepare($sql);
+    $stmt->bindParam(':id', $userID, PDO::PARAM_STR);
+    $res = $stmt->execute();
+    if ($res) {
+        $url = $_SERVER['HTTP_REFERER'];
+        $cart_products = $stmt->fetchAll();
+        // header('Location:' . $url);
+    }
+    $pdo = null;
+} catch (PDOException $e) {
 
-echo $e->getMessage();
+    echo $e->getMessage();
 }
-?>
