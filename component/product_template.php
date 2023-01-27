fd71f7050f3d0c91da8ac8d5ef3da679b054361e
hk-pG <hukada.gogotea@gmail.com>
2023-01-27 12:28:43 +0900

    商品一覧に正しい画像が表示されるように修正

--- component/product_template.php
+++ component/product_template.php
@@ -1,15 +1,18 @@
 <?php
 function product_template($row)
 {
-    $id = $row['id'];
-    // $link = "https://alumni.hamako-ths.ed.jp/~ei2031/shopping/page/product.php?id=$id";
-    $image_src = "../image/$id.png";
-    $product_name = $row['name'];
+	$id = $row['id'];
+	// $link = "https://alumni.hamako-ths.ed.jp/~ei2031/shopping/page/product.php?id=$id";
+	$image_src = "../image/$id.png";
+	$product_name = $row['name'];
 ?>
 
 <div class="product">
 	<a class="product-link" href="../page/product.php?id=<?= $id ?>" target="_blank">
-		<img class="product-img" src="<?= $image_src ?>" alt="<?= $product_name ?>">
+		<div>
+
+			<img class="product-img" src="<?= $image_src ?>" alt="<?= $product_name ?>">
+		</div>
 		<div>
 			<p class="product-title"><?= $product_name ?></p>
 		</div>
