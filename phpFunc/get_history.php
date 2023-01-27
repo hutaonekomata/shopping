f67c297ca35cde5161285498a7fb018f10e6bf23
Ryuta <ryuutafuruhashi310@gmail.com>
2023-01-27 12:29:24 +0900

    history could be got

---
+++ phpFunc/get_history.php
@@ -0,0 +1,29 @@
+<?php
+try{
+$userID = $_COOKIE['session_id'];
+$pdo_config = 'mysql:host=localhost;dbname=ei2031';
+
+$user='ei2031';
+$password='ei2031@alumni.hamako-ths.ed.jp';
+$option='[PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC]';
+
+$pdo = new PDO($pdo_config,$user,$password);
+$pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+
+$sql = '
+    select * from product inner join sales on product.id=sales.productID where userID=:id;
+';
+
+$stmt = $pdo->prepare($sql);
+$stmt->bindParam(':id',$userID,PDO::PARAM_STR);
+$res = $stmt->execute();
+if($res){
+    $data = $stmt->fetchAll();
+    $his_list=json_encode($data);
+}
+$pdo=null;
+}catch(PDOException $e){
+
+echo $e->getMessage();
+}
+?>
